0e4add531858d8c8adfe6ab03363a58ec7c21bb1
iammachine <iammachine@users.noreply.github.com>
2015-07-03 01:58:08 +0530

    update of gulpfile.js - commit.
    gulp.task('greet') - defined.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,3 +2,12 @@
  * Gulp: The streaming build system.
  * https://github.com/gulpjs/gulp | http://gulpjs.com/
  */
+
+var gulp = require('gulp');
+
+// DEFINING TASKS
+// To define a task, the gulp.task() function is used.
+// This function takes two attributes: the task’s name and a function to run.
+gulp.task('greet', function(){
+  console.log('Hello, World!');
+});
