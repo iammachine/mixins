b27ae5b80cb60538056aa320e1471bd3ea82c52e
iammachine <iammachine@users.noreply.github.com>
2015-07-04 00:19:59 +0530

    update of main.js - commit.

--- src/sass/script/main.js
+++ src/sass/script/main.js
@@ -1,4 +1,10 @@
 /*!
+ * @Mixins [v0.0.1] repository for every mixins.
  * JavaScript [v1.8.5] to Control Animations
  * =========================================
  */
+
+
+(function(){
+  // IIFE
+})();
