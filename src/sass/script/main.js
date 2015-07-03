94dd7f41294311f4fb02f16810e6ad8c6964cd14
iammachine <iammachine@users.noreply.github.com>
2015-07-04 00:30:59 +0530

    update of main.js - commit.
    initiating with "Mixins" global namespace and Immediately-Invoked Function Expression.

--- src/sass/script/main.js
+++ src/sass/script/main.js
@@ -1,10 +1,19 @@
 /*!
+ * -------------------------------------------------------
  * @Mixins [v0.0.1] repository for every mixins.
- * JavaScript [v1.8.5] to Control Animations
- * =========================================
+ * JavaScript [v1.8.5] to Control @Mixins
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : © 2015, Prabhat Kumar. All rights reserved.
+ * --------------------------------------------------------
  */
 
 
-(function(){
-  // IIFE
-})();
+/* @Define the "Mixins" global namespace. */
+window.Mixins = {};
+
+/* @Create and Define the Boot Module */
+/* @Initiating with Immediately-Invoked Function Expression [IIFE]. */
+(function(Mixins){
+  /* @Invoking strict mode */
+  "use strict";
+})(window.Mixins);
